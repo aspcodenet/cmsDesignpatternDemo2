57c08f5b76eaa55540f8956a349b3b0172280799
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-12-09 12:50:03 +0100

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    interface IMessageSender
+    {
+        void SendMessage(string message);
+    }
+
+    class SmsMessageSender : IMessageSender
+    {
+        public void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class EmailMessageSender : IMessageSender
+    {
+        public void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessageSender messageSender = new EmailMessageSender();
+            //..
+            //..
+            //if(sdadasadsa>)
+            messageSender.SendMessage("Hej hej");
+        }
+    }
+}
